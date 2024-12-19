e22cc52fd0acd393745929a6ea458cae0ef6e65b
Kevin <kitey13579@gmail.com>
2024-12-19 14:31:35 +0000

    temp splash screen and logo and some design changes

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include "KeyScan.h"
 #include <QtWidgets/QApplication>
+#include <QSplashScreen>
+#include "qtimer.h"
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QSplashScreen* splash = new QSplashScreen;
+    splash->setPixmap(QPixmap(".\\KeyScan.png"));
+
+    splash->show();
+
     KeyScan w;
-    w.show();
+    QTimer::singleShot(2000, splash, SLOT(close()));
+    QTimer::singleShot(2000, &w, SLOT(show()));
+
     return a.exec();
 }
