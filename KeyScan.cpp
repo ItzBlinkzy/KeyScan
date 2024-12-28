1a0d958e7c63bc545598bdf66c7b562f11f04033
Kevin <kitey13579@gmail.com>
2024-12-28 05:03:08 +0000

    fixed some modifier key issues

--- KeyScan.cpp
+++ KeyScan.cpp
@@ -6,34 +6,6 @@
 #include <Windows.h>
 #include <QKeyEvent>
 #include <QTimer>
-struct KeyMapping {
-    QString keyText;
-    uint16_t virtualKey;
-};
-
-const QList<QList<KeyMapping>> keyboard_layout = {
-    {{"Esc", VK_ESCAPE}, {"F1", VK_F1}, {"F2", VK_F2}, {"F3", VK_F3}, {"F4", VK_F4}, {"F5", VK_F5},
-     {"F6", VK_F6}, {"F7", VK_F7}, {"F8", VK_F8}, {"F9", VK_F9}, {"F10", VK_F10}, {"F11", VK_F11}, {"F12", VK_F12}},
-    {{"`", VK_OEM_8}, {"1", '1'}, {"2", '2'}, {"3", '3'}, {"4", '4'}, {"5", '5'}, {"6", '6'},
-     {"7", '7'}, {"8", '8'}, {"9", '9'}, {"0", '0'}, {"-", VK_OEM_MINUS}, {"=", VK_OEM_PLUS}, {"Backspace", VK_BACK}},
-    {{"Tab", VK_TAB}, {"Q", 'Q'}, {"W", 'W'}, {"E", 'E'}, {"R", 'R'}, {"T", 'T'}, {"Y", 'Y'},
-     {"U", 'U'}, {"I", 'I'}, {"O", 'O'}, {"P", 'P'}, {"[", VK_OEM_4}, {"]", VK_OEM_6}, {"\\", VK_OEM_5}},
-    {{"Caps Lock", VK_CAPITAL}, {"A", 'A'}, {"S", 'S'}, {"D", 'D'}, {"F", 'F'}, {"G", 'G'}, {"H", 'H'},
-     {"J", 'J'}, {"K", 'K'}, {"L", 'L'}, {";", VK_OEM_1}, {"'", VK_OEM_3}, {"Enter", VK_RETURN}},
-    {{"Left Shift", VK_LSHIFT}, {"Z", 'Z'}, {"X", 'X'}, {"C", 'C'}, {"V", 'V'}, {"B", 'B'}, {"N", 'N'},
-     {"M", 'M'}, {",", VK_OEM_COMMA}, {".", VK_OEM_PERIOD}, {"/", VK_OEM_2}, {"Right Shift", VK_RSHIFT}},
-    {{"Left Ctrl", VK_LCONTROL}, {"Windows", VK_LWIN}, {"Left Alt", VK_LMENU}, {"Space", VK_SPACE},
-     {"Right Alt", VK_RMENU}, {"Menu", VK_APPS}, {"Right Ctrl", VK_RCONTROL}},
-    {{"Up", VK_UP}},
-    {{"Left", VK_LEFT}, {"Down", VK_DOWN}, {"Right", VK_RIGHT}},
-    {{"INS", VK_INSERT}, {"HOME", VK_HOME}, {"PG UP", VK_PRIOR}},
-    {{"DEL", VK_DELETE}, {"END", VK_END}, {"PG DWN", VK_NEXT}},
-    //{{"Num Lock", VK_NUMLOCK}, {"Numpad /", VK_DIVIDE}, {"Numpad *", VK_MULTIPLY}, {"Numpad -", VK_SUBTRACT},
-    // {"Numpad 7", VK_NUMPAD7}, {"Numpad 8", VK_NUMPAD8}, {"Numpad 9", VK_NUMPAD9}, {"Numpad +", VK_ADD},
-    // {"Numpad 4", VK_NUMPAD4}, {"Numpad 5", VK_NUMPAD5}, {"Numpad 6", VK_NUMPAD6},
-    // {"Numpad 1", VK_NUMPAD1}, {"Numpad 2", VK_NUMPAD2}, {"Numpad 3", VK_NUMPAD3},
-    // {"Numpad 0", VK_NUMPAD0}, {"Numpad .", VK_DECIMAL}}
-};
 
 RecentKeyList recent_keys;
 
@@ -43,9 +15,6 @@ KeyScan::KeyScan(QWidget *parent)
 {
     this->setFixedSize(1280, 720);
     ui.setupUi(this);
-    ui.NavbarLayout->setAlignment(Qt::AlignTop);
-
-
 
     QList<QHBoxLayout*> layouts = {
          ui.f1_layout,
@@ -65,6 +34,7 @@ KeyScan::KeyScan(QWidget *parent)
         for (const KeyMapping& key : keyboard_layout[i]) {
             QPushButton* button = new QPushButton(key.keyText); 
             layouts[i]->addWidget(button);  
+            button->setFocusPolicy(Qt::NoFocus);
             buttons[key.virtualKey] = button;
         }
     }
@@ -72,8 +42,30 @@ KeyScan::KeyScan(QWidget *parent)
 
 
 QString findKeyText(uint16_t virtualKey, const QList<QList<KeyMapping>>& layout) {
-
-
+    if (virtualKey == VK_SHIFT) {
+        if ((GetKeyState(VK_LSHIFT) & 0x8000)) {
+            virtualKey = VK_LSHIFT;
+        }
+        else if ((GetKeyState(VK_RSHIFT) & 0x8000)) {
+            virtualKey = VK_RSHIFT;
+        }
+    }
+    else if (virtualKey == VK_CONTROL) {
+        if ((GetKeyState(VK_LCONTROL) & 0x8000)) {
+            virtualKey = VK_LCONTROL;
+        }
+        else if ((GetKeyState(VK_RCONTROL) & 0x8000)) {
+            virtualKey = VK_RCONTROL;
+        }
+    }
+    else if (virtualKey == VK_MENU) {
+        if ((GetKeyState(VK_LMENU) & 0x8000)) {
+            virtualKey = VK_LMENU;
+        }
+        else if ((GetKeyState(VK_RMENU) & 0x8000)) {
+            virtualKey = VK_RMENU;
+        }
+    }
 
     for (const auto& row : layout) {
         for (const auto& key : row) {
@@ -82,8 +74,10 @@ QString findKeyText(uint16_t virtualKey, const QList<QList<KeyMapping>>& layout)
             }
         }
     }
+
     return QString("UNK");
 }
+
 
 
 void KeyScan::keyPressEvent(QKeyEvent* event) {
@@ -128,7 +122,6 @@ void KeyScan::keyPressEvent(QKeyEvent* event) {
         QPushButton* button = buttons[virtual_key];
         modifyButtonStyle(button, "background-color: #ffcc2c;");
     }
-    QMainWindow::keyPressEvent(event);
 }
 
 
@@ -173,17 +166,9 @@ void KeyScan::keyReleaseEvent(QKeyEvent* event) {
         modifyButtonStyle(button, "background-color: #00635f;color: white;");
 
     }
-    QMainWindow::keyPressEvent(event);
 }
 void KeyScan::modifyButtonStyle(QPushButton* button, QString stylesheet) {
     button->setStyleSheet(stylesheet);
-}
-
-void KeyScan::updateLastKeyPressedText(QLabel* label, uint16_t virtual_key) {
-
-}
-
-void KeyScan::updateTotalWorkingKeys(QLabel* label, uint16_t working_key_count) {
 }
 
 
--- KeyScan.h
+++ KeyScan.h
@@ -9,7 +9,10 @@
 #include <QLayout>
 #include <QtWidgets/QMainWindow>
 #include "ui_KeyScan.h"
+#include <Windows.h>
 #include "qlabel.h"
+
+
 class KeyScan : public QMainWindow
 {
     Q_OBJECT
@@ -21,6 +24,7 @@ public:
     void modifyButtonStyle(QPushButton* button, QString stylesheet);
     void updateLastKeyPressedText(QLabel* label, uint16_t virtual_key);
     void updateTotalWorkingKeys(QLabel* label, uint16_t working_key_count);
+    quint32 resolveModifierKey(quint32 virtual_key, quint32 scanCode);
     ~KeyScan();
 
 private:
@@ -38,7 +42,7 @@ public:
         
         while (layout->count() >= MAX_SIZE) {
             // remove and delete the oldest widget if reached max_size
-            QLayoutItem* item = layout->takeAt(layout->count() - 1);
+            QLayoutItem* item = layout->takeAt(0);
             if (item) {
                 delete item->widget(); 
                 delete item;           
@@ -47,6 +51,35 @@ public:
 
         // add most recent key pressedTHYH
         QPushButton* button = new QPushButton(value);
-        layout->insertWidget(0, button);
+        layout->addWidget(button);
     }
+};
+
+struct KeyMapping {
+    QString keyText;
+    uint16_t virtualKey;
+};
+
+const QList<QList<KeyMapping>> keyboard_layout = {
+    {{"Esc", VK_ESCAPE}, {"F1", VK_F1}, {"F2", VK_F2}, {"F3", VK_F3}, {"F4", VK_F4}, {"F5", VK_F5},
+     {"F6", VK_F6}, {"F7", VK_F7}, {"F8", VK_F8}, {"F9", VK_F9}, {"F10", VK_F10}, {"F11", VK_F11}, {"F12", VK_F12}},
+    {{"`", VK_OEM_8}, {"1", '1'}, {"2", '2'}, {"3", '3'}, {"4", '4'}, {"5", '5'}, {"6", '6'},
+     {"7", '7'}, {"8", '8'}, {"9", '9'}, {"0", '0'}, {"-", VK_OEM_MINUS}, {"=", VK_OEM_PLUS}, {"Backspace", VK_BACK}},
+    {{"Tab", VK_TAB}, {"Q", 'Q'}, {"W", 'W'}, {"E", 'E'}, {"R", 'R'}, {"T", 'T'}, {"Y", 'Y'},
+     {"U", 'U'}, {"I", 'I'}, {"O", 'O'}, {"P", 'P'}, {"[", VK_OEM_4}, {"]", VK_OEM_6}, {"\\", VK_OEM_5}},
+    {{"Caps Lock", VK_CAPITAL}, {"A", 'A'}, {"S", 'S'}, {"D", 'D'}, {"F", 'F'}, {"G", 'G'}, {"H", 'H'},
+     {"J", 'J'}, {"K", 'K'}, {"L", 'L'}, {";", VK_OEM_1}, {"'", VK_OEM_3}, {"Enter", VK_RETURN}},
+    {{"Left Shift", VK_LSHIFT}, {"Z", 'Z'}, {"X", 'X'}, {"C", 'C'}, {"V", 'V'}, {"B", 'B'}, {"N", 'N'},
+     {"M", 'M'}, {",", VK_OEM_COMMA}, {".", VK_OEM_PERIOD}, {"/", VK_OEM_2}, {"Right Shift", VK_RSHIFT}},
+    {{"Left Ctrl", VK_LCONTROL}, {"Windows", VK_LWIN}, {"Left Alt", VK_LMENU}, {"Space", VK_SPACE},
+     {"Right Alt", VK_RMENU}, {"Menu", VK_APPS}, {"Right Ctrl", VK_RCONTROL}},
+    {{"Up", VK_UP}},
+    {{"Left", VK_LEFT}, {"Down", VK_DOWN}, {"Right", VK_RIGHT}},
+    {{"INS", VK_INSERT}, {"HOME", VK_HOME}, {"PG UP", VK_PRIOR}},
+    {{"DEL", VK_DELETE}, {"END", VK_END}, {"PG DWN", VK_NEXT}},
+    //{{"Num Lock", VK_NUMLOCK}, {"Numpad /", VK_DIVIDE}, {"Numpad *", VK_MULTIPLY}, {"Numpad -", VK_SUBTRACT},
+    // {"Numpad 7", VK_NUMPAD7}, {"Numpad 8", VK_NUMPAD8}, {"Numpad 9", VK_NUMPAD9}, {"Numpad +", VK_ADD},
+    // {"Numpad 4", VK_NUMPAD4}, {"Numpad 5", VK_NUMPAD5}, {"Numpad 6", VK_NUMPAD6},
+    // {"Numpad 1", VK_NUMPAD1}, {"Numpad 2", VK_NUMPAD2}, {"Numpad 3", VK_NUMPAD3},
+    // {"Numpad 0", VK_NUMPAD0}, {"Numpad .", VK_DECIMAL}}
 };
